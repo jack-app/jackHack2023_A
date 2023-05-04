2abbb797c67272c43e997711a36099fab9215521
すけ <131859121+suke-0612@users.noreply.github.com>
2023-05-04 15:32:21 +0900

    タイトルを作成

--- frontend/src/views/TitlePage.vue
+++ frontend/src/views/TitlePage.vue
@@ -1,30 +1,39 @@
 <template>
-    <div class="d-flex_align-center_flex-column">
+    <div>
         <v-card
-    class="mx-auto"
-    max-width="450"
-    variant="outlined"
-  >
-    <v-card-item>
-      <div>
-    
-        <div class="text-h6 mb-1">
-          --ルール説明--
-        </div>
-        <div class="text-caption">
-            <div class="ruleteller-title">
-            <p>みんなが出した単語を使ってロマンチックな文章を作ろう！</p>
-            <p>1. お題に合わせて好きな単語を打ち込んでください！</p> 
-            <p>2. ５人それぞれが選んだ単語を使ってロマンチックな文章を作りましょう！</p>
-            <p>3. 一番ロマンチックな文章を書いた人に投票しよう！</p>
-        </div>
-       </div>
-      </div>
-    </v-card-item>
-
-  </v-card>
+        style="border-width: 10px;border-color:#C08A9B"
+        class="mx-auto"
+        width="80%"
+        height="auto"
+        outlined
+        >
+            <v-card-item>
+                <div class="text-h6 mb-1">
+                    <p class="ruletellerT-title">--ルール説明--</p>
+                </div>
+                <div class="div-equal-box-title">
+                    <p class="ruletellerM-title">みんなが出した単語を使ってロマンチックな文章を作ろう！</p>
+                    <p class="ruletellerM-title">1. お題に合わせて好きな単語を打ち込んでください！</p>
+                    <p class="ruletellerM-title">2. ５人それぞれが選んだ単語を使ってロマンチックな文章を作りましょう！</p>
+                    <p class="ruletellerM-title">3. 一番ロマンチックな文章を書いた人に投票しよう！</p>
+                </div>
+            </v-card-item>
+        </v-card>
+        <v-container class="text-center">
+            <v-row justify="center">
+                <v-col cols="12" sm="6" md="4">
+                    <v-btn
+                    height="100px"
+                    block color="#C08A9B"
+                    size="large">
+                        <div class="buttonGS-title">GAME START</div>
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+        <input v-model="message" placeholder="edit me">
+<p>Message is: {{ message }}</p>
     </div>
-
 </template>
 
 
@@ -45,14 +54,29 @@ data: () => ({
 </script>
   
 <style>
-.d-flex_align-center_flex-column{
-    display: flex;
-  justify-content: center;
-  align-items: center;
-    /* width: 100%;
-  margin: 0 auto;
-  max-width: 500px; */
+
+.ruletellerM-title{
+    color:#C08A9B;
+    font-size:35px;
 }
-.ruleteller-title{
+.text-center{
+    position:relative;
+    left:26%;
+    top:3%;
+}
+.buttonGS-title{
+    font-size: 70px;
+
+    color:white;
+}
+.ruletellerT-title{
+    color:#C08A9B;
+    font-size:50px;
+    margin:15px;
+}
+.div-equal-box-title{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 }
 </style>
