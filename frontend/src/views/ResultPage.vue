cd033e7c303d877e6c2559363b96fbb703187165
YutaK1026 <inheritans1026@gmail.com>
2023-05-04 14:07:54 +0900

    add ResultPageにaxios回りの設定を追加

--- frontend/src/views/ResultPage.vue
+++ frontend/src/views/ResultPage.vue
@@ -4,23 +4,34 @@
             結果発表～～
         </p>
         <p class = "result-2">
-            成功です！！！
+            {{ result }}の勝ち！
         </p>
     </div>
 </template>
   
 <script>
-
+import axios from "axios";
 export default {
-name: 'ResultPage',
+    name: 'ResultPage',
+    components: {
+        
+    },
 
-components: {
-    
-},
-
-data: () => ({
-    
-}),
+    data(){
+        return{
+            result: "",
+        }
+    },
+    created(){
+        const room_id = localStorage.getItem('room_id');
+        axios.post("http://localhost:3000/api/get_result",{room_id: room_id})
+        .then((res) => {
+            this.result = res.data.result;
+        })
+    },
+    methods:{
+        
+    }
 };
 </script>
 
