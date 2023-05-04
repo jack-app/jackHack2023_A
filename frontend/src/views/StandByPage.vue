c4205f112c7e41213166723fbf90b9adf85a254c
すけ <131859121+suke-0612@users.noreply.github.com>
2023-05-04 10:43:17 +0900

    add タイトルページとスタンバイページの作成

--- frontend/src/views/StandByPage.vue
+++ frontend/src/views/StandByPage.vue
@@ -1,9 +1,15 @@
 <template>
-    <dev>
-        文章
-    </dev>
+   <div class="container-standby">
+        <div class="maintext-standby">
+            NOW LOADING ...
+        </div>
+        
+        <div class="subtext-standby">
+            参加者を集めています
+        </div>
+    </div>
 </template>
-  
+
 <script>
 
 export default {
@@ -18,4 +24,26 @@ data: () => ({
 }),
 };
 </script>
-  
+  <style>
+.maintext-standby{
+    font-size: 100px;
+    color:#C08A9B;
+    text-align: center;
+    /* position: absolute;
+  top: 50%;
+  left: 50%; */
+  /* margin: -25px 0 0 -25px; */
+}
+.container-standby{
+    width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.subtext-standby{
+color:#C08A9B;
+font-size:xx-large;
+}
+</style>
