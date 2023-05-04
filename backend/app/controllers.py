19ca917230856cb95da74d7c528c4bd8c05271e5
sakoma33 <dev7618jifekd@gmail.com>
2023-05-04 15:36:01 +0900

    スタートの処理を実装途中

--- backend/app/controllers.py
+++ backend/app/controllers.py
@@ -1,5 +1,5 @@
-import string
 import random
+import string
 from datetime import datetime
 
 import eventlet
@@ -38,8 +38,9 @@ from starlette.websockets import WebSocket
 app_fastapi = FastAPI()
 sio = socketio.AsyncServer(async_mode='asgi')
 # sio.register_namespace(MyCustomNamespace('/socket.io'))  # 名前空間を設定
-app_socketio = socketio.ASGIApp(sio, other_asgi_app=app_fastapi)  # wsgiサーバーミドルウェア生成
-# eventlet.wsgi.server(eventlet.listen(('localhost', 3000)), app_asgi)  # wsgiサーバー起動
+# app_socketio = socketio.ASGIApp(sio, other_asgi_app=app_fastapi)  # wsgiサーバーミドルウェア生成
+app_socketio = socketio.WSGIApp(sio)  # wsgiサーバーミドルウェア生成
+eventlet.wsgi.server(eventlet.listen(('localhost', 3000)), app_socketio)  # wsgiサーバー起動
 
 
 origins = [
@@ -63,6 +64,11 @@ async def index(request: Request):
     return views.TemplateResponse('sample.html', {'request': request})
 
 
+@app_fastapi.get("/matchwaiting")
+async def match_wating(request: Request):
+    return views.TemplateResponse('sample.html')
+
+
 @app_fastapi.post("/api/input_word")
 async def input_word(request: Request):
 
@@ -79,11 +85,80 @@ async def ping(sid: str):
     return {"result": "OK"}
 
 
+stand_by_player = []
+
+
+@sio.on("start")
+async def start(sid, player_id):
+    stand_by_player.append([player_id, sid])
+    print("現在の待機プレイヤー：" + len(stand_by_player) + "名")
+
+    def join_room(players):
+        # 長さ10のランダムなアルファベットと数字の組み合わせを生成する
+        room_id = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        print(room_id)
+        for player in players:
+            sio.enter_room(sid=player[1], room=room_id)
+
+        sio.emit("room-full", room=room_id)
+
+    if len(stand_by_player) >= 5:
+        join_room(stand_by_player[0:5])
+        del stand_by_player[0:5]
+    return None
+
+
 @sio.event
 async def connect(sid, environ):
     """socketioのconnectイベント
     """
     print('connect ', sid)
+
+    # standByPlayer = []
+
+    # @sio.on("AutoMatchingPreLogin")
+    # async def auto_matching_pre_login(player_id):
+    #     standByPlayer.append(player_id)
+    #     print("現在の待機プレイヤー：" + len(standByPlayer) + "名")
+
+    #     def joinRoom(player1, player2, player3, player4, player5):
+    #         # 長さ10のランダムなアルファベットと数字の組み合わせを生成する
+    #         room_id = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+    #         print(room_id)
+    #         sio.emit("FullRoom", room_id)
+    #         if len(standByPlayer) >= 5:
+    #             player1 = standByPlayer[0]
+    #             player2 = standByPlayer[1]
+    #             player3 = standByPlayer[2]
+    #             player4 = standByPlayer[3]
+    #             player5 = standByPlayer[4]
+    #             del standByPlayer[0:5]
+    #             joinRoom(player1, player2, player3, player4, player5)
+
+    # @sio.on("LeaveWaitingRoom")
+    # async def leave_waiting_room(player_id):
+    #     idx = standByPlayer.index(player_id)
+    #     del standByPlayer[idx]
+    #     print("現在の待機プレイヤー：" + len(standByPlayer) + "名")
+    #     print(standByPlayer)
+
+    # num_clients = {}
+    # # num_player = {}
+
+    # @sio.on("login")
+    # async def login(room_id):
+    #     if not room_id in num_clients:
+    #         num_clients[room_id] = 1
+    #     else:
+    #         num_clients[room_id] += 1
+
+    #     if num_clients[room_id] > 5:
+    #         print("This room is full.")
+    #     else:
+    #         sio.enter_room(room=room_id)
+    #         print("roomに入室が完了しました")
+    #         print(room_id)
+    #         print("今のroomにいる人数"+num_clients[room_id])
 
 
 @sio.event
@@ -91,36 +166,3 @@ async def disconnect(sid):
     """socketioのdisconnectイベント
     """
     print('disconnect ', sid)
-
-
-standByPlayer = []
-
-
-@sio.on("AutoMatchingPreLogin")
-async def auto_matching_pre_login(playerId):
-    standByPlayer.append(playerId)
-    print("現在の待機プレイヤー：" + len(standByPlayer) + "名")
-
-    def joinRoom(player1, player2, player3, player4):
-        # 長さ10のランダムなアルファベットと数字の組み合わせを生成する
-        room_id = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-        print(room_id)
-        sio.emit("FullRoom", room_id)
-
-
-io.sockets.on("connection", function(socket) {
-  console.log("connected");
-  // オートマッチング機能
-  socket.on("AutoMatchingPreLogin", function(playerId) {
-    standByPlayer.push(playerId);
-    console.log("現在の待機プレイヤー：" + standByPlayer.length + "名");
-    function joinRoom(player1, player2) {
-      let roomId=Math.random().toString(32).substring(2);
-      io.emit("FullRoom", roomId, player1, player2); }
-    if (standByPlayer.length >= 2) {
-      let player1=standByPlayer[0];
-      let player2=standByPlayer[1];
-      standByPlayer.splice(0, 2);
-      setTimeout(function() {
-        joinRoom(player1, player2); }, 1000);     }
-  });
