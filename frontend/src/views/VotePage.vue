287c956d723cba64095bce7bd2ef20cabdcbc8cd
sattsunn <satsusarii@gmail.com>
2023-05-04 15:12:58 +0900

    ページ全体一通り完成文章はまだ

--- frontend/src/views/VotePage.vue
+++ frontend/src/views/VotePage.vue
@@ -1,10 +1,33 @@
 <template>
-    <div>
-        おはよう
-        こんにちは
-    </div>
+    <div class="vote-all">
+        <div class="wrapper">
+         
+          <div class="oneone"><img class="hidari" src="../assets/hidariok.jpg" alt=""></div>
+          <div class="onetwo">aiueokaki</div>
+          <div class="onethree"><img class="migi" src="../assets/migiiii.jpg" alt=""></div>
+          <div class="onefour"><button type="button" name="name" value="value">vote</button></div>
+
+          <div class="oneone"><img class="hidari" src="../assets/hidariok.jpg" alt=""></div>
+          <div class="onetwo"></div>
+          <div class="onethree"><img class="migi" src="../assets/migiiii.jpg" alt=""></div>
+          <div class="onefour"><button type="button" name="name" value="value">vote</button></div>
+
+          <div class="oneone"><img class="hidari" src="../assets/hidariok.jpg" alt=""></div>
+          <div class="onetwo"></div>
+          <div class="onethree"><img class="migi" src="../assets/migiiii.jpg" alt=""></div>
+          <div class="onefour"><button type="button" name="name" value="value">vote</button></div>
+
+          <div class="oneone"><img class="hidari" src="../assets/hidariok.jpg" alt=""></div>
+          <div class="onetwo"></div>
+          <div class="onethree"><img class="migi" src="../assets/migiiii.jpg" alt=""></div>
+          <div class="onefour"><button type="button" name="name" value="value">vote</button></div>
+          
+       </div>
+ </div>
+   
 </template>
   
+
 <script>
 
 export default {
@@ -20,3 +43,45 @@ data: () => ({
 };
 </script>
   
+<style>
+ 
+
+.wrapper {
+  table-layout: fixed;
+  margin: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: 200px;
+  
+   }
+   .onetwo{
+    table-layout: fixed;
+    font-size: 50px;
+    text-align: center;
+    padding: 40px;
+
+}
+
+
+.onefour{
+    margin: 30px;
+    padding: 10px;
+    text-align: center;
+    font-size: 70px;
+    color: #FFFFFF;
+    border-bottom: 1px solid;
+    background-color: #C08A9B;
+
+    
+}
+
+.hidari{
+    width:300px;
+    height:170px;
+}
+
+.migi{
+    width:300px;
+    height:170px; 
+}
+</style>
